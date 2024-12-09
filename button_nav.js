9123ddca6da078a42db333b3fdad45bd891a2965
John Khalife <jpa.khalife@gmail.com>
2024-12-09 02:44:49 -0500

    deleted more unneccessary files

--- button_nav.js
+++
@@ -1,9 +0,0 @@
-/**
- *  This file is intended to create button functionality in ultimate tic tact toe
- *  This game is can only be interacted with through arrows and keys on the keyboard, so this class is intented to create an array of buttons to be able to find the closest button to switch to.
- *  
- * 
- * 
- * 
- * 
- */
