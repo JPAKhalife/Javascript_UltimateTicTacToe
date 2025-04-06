3a7ed930d8f4c706b1ff5cf23cb2a8de61d584d6
JPAKhalife <jpa.khalife@gmail.com>
2025-04-06 15:40:24 -0400

    Fix: The tutorial screen now works properly.

--- src/FrontEnd/Screens/TutorialScreen.ts
+++ src/FrontEnd/Screens/TutorialScreen.ts
@@ -7,7 +7,6 @@
  */
 
 import p5 from "p5";
-import Cutscene from "../Cutscene";
 import GuiManager from "../GuiManager";
 import KeyListener, { KEY_EVENTS } from "../KeyListener";
 import Menu, { Screens } from "../Menu"
@@ -21,26 +20,26 @@ export default class TutorialScreen implements Menu {
     private sketch: p5
     private keylistener: KeyListener;
     private screenState: [number, number];
-    private changeScreen: Cutscene;
     private tutorialImages: Img[];
     private paragraphs: ShapeGroup[];
+    private transitioning: boolean;
+    private transitionIn: boolean;
 
     constructor(sketch: p5) {
         this.sketch = sketch;
         this.keylistener = new KeyListener(this.sketch);
         //There are multiple screens in one for this screen. We will have a single cutscene that is used to transition between all of them.
-        this.screenState = [0,0];
-    
+        this.screenState = [0, 0]; // [current screen index, opacity]
         //This array is used for all of the images on each screen
         this.tutorialImages = [
-            new Img(tictacboard, getCanvasSize()/2, getCanvasSize()/20*9,this.sketch,getCanvasSize()*0.4,getCanvasSize()*0.4),
-            new Img(tictacboard_two, getCanvasSize()/2, getCanvasSize()/20*9,this.sketch,getCanvasSize()*0.4,getCanvasSize()*0.4),
-            new Img(tictacboard_three,getCanvasSize()/2,getCanvasSize()/20*9,this.sketch,getCanvasSize()*0.4,getCanvasSize()*0.4)
+            new Img(tictacboard,getCanvasSize()*0.4,getCanvasSize()*0.4 ,this.sketch, getCanvasSize()/2, getCanvasSize()/20*9),
+            new Img(tictacboard_two, getCanvasSize()*0.4,getCanvasSize()*0.4,this.sketch,getCanvasSize()/2, getCanvasSize()/20*9),
+            new Img(tictacboard_three,getCanvasSize()*0.4,getCanvasSize()*0.4,this.sketch,getCanvasSize()/2,getCanvasSize()/20*9)
         ];
         for (let i = 0 ; i < this.tutorialImages.length ; i++) {
             this.tutorialImages[i].setRectOrientation(this.sketch.CENTER);
             this.tutorialImages[i].setImageOrientation(this.sketch.CENTER);
-            this.tutorialImages[i].setTint(sketch.color(0));
+            this.tutorialImages[i].setTint(this.sketch.color(255,255));
         }
     
         //This array will contain all of the paragraphs for each screen
@@ -62,71 +61,60 @@ export default class TutorialScreen implements Menu {
             new Text('Press space to continue.',getCanvasSize()/2,getCanvasSize()/5*4,this.sketch))
         ]
         for (let i = 0 ; i < this.paragraphs.length ; i++) {
-            this.paragraphs[i].callFunctionOnAll("setFill",255,255,255,0);
+            this.paragraphs[i].callFunctionOnAll("setFill", this.sketch.color(255,255,255,0));
             this.paragraphs[i].callFunctionOnAll("setTextSize",getCanvasSize()*0.02);
             this.paragraphs[i].callFunctionOnAll("setFont",fontOSDMONO);
             this.paragraphs[i].callFunctionOnAll("setRectOrientation",this.sketch.CENTER);
             this.paragraphs[i].callFunctionOnAll("setTextOrientation",this.sketch.CENTER,this.sketch.CENTER);
         }
     
-        //Now we need a transition animation for when the user presses space.
-        this.changeScreen = new Cutscene(this.keylistener,this.tutorialImages,this.paragraphs,this.screenState,true);
-    
-        this.changeScreen.setCondition(() => {
-            if (this.keylistener.listen() == KEY_EVENTS.SELECT && !Cutscene.isPlaying) {
-                this.keylistener.deactivate();
-                this.changeScreen.activate()
-            }
-        });
-    
-        this.changeScreen.setAnimation(() => {
-            //Check if we are transitioning in
-            if (this.changeScreen.getShape(3)) {
-                //If we are, fade in
-                if (this.changeScreen.getShape(2).opacity < 255) {
-                    this.changeScreen.getShape(2).opacity += 255/TUTORIAL_SCREEN_TRANSITION_TIME;
+        this.transitioning = false;
+        this.transitionIn = true;
+        this.startTransition();
+    }
+
+    private handleTransition(): void {
+        if (this.transitioning) {
+            if (this.transitionIn) {
+                if (this.screenState[1] < 255) {
+                    this.screenState[1] += 255 / TUTORIAL_SCREEN_TRANSITION_TIME;
                 } else {
-                    //Once we have completely faded in, set the transition in to false,
-                    //And end the animation.
-                    this.changeScreen.setShape(3,false);
-                    this.changeScreen.deactivate();
+                    this.transitionIn = false;
+                    this.transitioning = false;
                     this.keylistener.activate();
                 }
             } else {
-                //We are transitioning out
-                if (this.changeScreen.getShape(2).opacity > 0) {
-                    this.changeScreen.getShape(2).opacity -= 255/TUTORIAL_SCREEN_TRANSITION_TIME;
+                if (this.screenState[1] > 0) {
+                    this.screenState[1] -= 255 / TUTORIAL_SCREEN_TRANSITION_TIME;
                 } else {
-                    //Once we have faded out, it is time to fade in.
-                    this.changeScreen.setShape(3,true);
-                    if (this.changeScreen.getShape(2).value >= this.changeScreen.getShape(0).length - 1) {
-                        //If we are at the end, change the screen
-                        this.changeScreen.deactivate();
-                        this.keylistener.activate();
-                        GuiManager.changeScreen(Screens.SETUP_SCREEN,this.sketch);
+                    this.transitionIn = true;
+                    if (this.screenState[0] >= this.tutorialImages.length - 1) {
+                        GuiManager.changeScreen(Screens.SETUP_SCREEN, this.sketch);
                     } else {
-                        this.changeScreen.getShape(2).opacity = 0;
-                        this.changeScreen.getShape(0)[this.changeScreen.getShape(2).value].setTint(0);
-                        this.changeScreen.getShape(1)[this.changeScreen.getShape(2).value].callFunction('setFill',255,255,255,0);
-                        this.changeScreen.getShape(2).value++;
+                        this.screenState[1] = 0;
+                        this.screenState[0]++;
                     }
                 }
             }
-            //While the animation is going, set the tint no matter what
-            this.changeScreen.getShape(0)[this.changeScreen.getShape(2).value].setTint(this.changeScreen.getShape(2).opacity);
-            this.changeScreen.getShape(1)[this.changeScreen.getShape(2).value].callFunction('setFill',255,255,255,this.changeScreen.getShape(2).opacity);
-        });
-    
-        this.changeScreen.activate();
+        }
+    }
+
+    private startTransition(): void {
+        this.transitioning = true;
+        this.keylistener.deactivate();
     }
 
     public draw(): void {
         this.sketch.background(0);
-        //Render the proper image and text
+        this.tutorialImages[this.screenState[0]].setTint(this.sketch.color(255, this.screenState[1]));
         this.tutorialImages[this.screenState[0]].render();
-        this.paragraphs[this.screenState[0]].callFunctionOnAll('render');
-        //Listen for the transition animation
-        this.changeScreen.listen();
+        this.paragraphs[this.screenState[0]].callFunctionOnAll("setFill", this.sketch.color(255, this.screenState[1]));
+        this.paragraphs[this.screenState[0]].callFunctionOnAll("render");
+        this.handleTransition();
+
+        if (this.keylistener.listen() === KEY_EVENTS.SELECT && !this.transitioning) {
+            this.startTransition();
+        }
     }
 
     public resize(): void {
